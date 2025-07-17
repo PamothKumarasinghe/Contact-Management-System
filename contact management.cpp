df69e73b180d33df175552d12baf1b00bfee0d03
PamothKumarasinghe <nikolailaazz2@gmail.com>
2025-07-17 08:47:37 +0530

    add, view

--- contact management.cpp
+++ contact management.cpp
@@ -1,12 +1,54 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
 using namespace std;
+
+// store the contact as 
+// nikolai laazz|0742614052|nikolai@example.com
+// in the file contacts.txt
 
 struct Contact {
     string name;
     string phone;
     string email;
 };
+vector<string> split(string& line) {
+    
+}
+void addContact() {
+    Contact c;
+    cout << "Enter name:\n ";
+    getline(cin, c.name);
 
+    cout << "Enter phone number: \n";
+    getline(cin, c.phone);
+
+    cout << "Enter email: \n";
+    getline(cin, c.email);
+
+    ofstream file("contact.txt", ios::app);
+    if (file.is_open()) 
+        file << c.name <<"|" << c.phone << "|" << c.email;
+    file.close();
+
+    cout << "Contact added successfully!" << endl;
+}
+void viewContact() {
+    ifstream file("contacts.txt");
+    string line;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            vector<string> data = split(line);
+
+            if (data.size() == 3) {
+                cout << data[0] << " " << data[1] << " " << data[2] << endl;
+            }
+        }
+    }
+    file.close();
+}
 int main() {
 
 }
